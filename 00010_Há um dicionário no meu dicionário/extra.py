2e6361a451995f7e6421f11607aad344302a6bb6
rocio@mumuki.org <rocio@mumuki.org>
2022-08-25 20:23:50 +0000

    Mumuki Export on 2022-08-25 20:23:50 +0000

---
+++ 00010_Há um dicionário no meu dicionário/extra.py
@@ -0,0 +1,11 @@
+menu_do_dia = {
+  "prato_principal": "berinjelas à milanesa",
+  "salada": ["batata", "cenoura", "ervilhas"],
+  "sobremesa": { "ingredientes": ["cream cheese", "framboesas"], "tempo_de_cozimento": 80 }
+}
+
+menu_infantil = {
+  "prato_principal": "pizza",
+  "salada": ["lechuga", "tomate", "zanahoria"],
+  "sobremesa": { "ingredientes": ["galletitas", "dulce de leche", "crema"], "tempo_de_cozimento": 20 }
+}
---
+++ 00010_Há um dicionário no meu dicionário/hint.md
@@ -0,0 +1,5 @@
+Vamos ver um exemplo. Se você quiser saber os `ingredientes` da `sobremesa` do `menu_do_dia` você deve fazer:
+
+``` python
+ム menu_do_dia["sobremesa"]["ingredientes"]
+```
