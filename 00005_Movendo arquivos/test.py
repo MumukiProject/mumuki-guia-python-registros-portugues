ae001b0175b92e62bc0455d5f4ea4ffcd84913de
rocio@mumuki.org <rocio@mumuki.org>
2022-08-25 20:43:07 +0000

    Mumuki Export on 2022-08-25 20:43:07 +0000

--- 00005_Movendo arquivos/test.py
+++ 00005_Movendo arquivos/test.py
@@ -1,15 +1,15 @@
 
   def test_mover_archivo_le_cambia_la_ruta_a_un_archivo_al_pasar_una_ruta_nueva(self):
-    archivo = {"ruta":"/usr/miarchivo.doc", "creacion":"15/02/2019"}
+    archivo = {"percurso":"/usr/miarchivo.doc", "criacao":"15/02/2019"}
     mover_arquivo(archivo, "/home/miarchivo.doc")
-    self.assertEqual(archivo["ruta"], "/home/miarchivo.doc")
+    self.assertEqual(archivo["percurso"], "/home/miarchivo.doc")
 
   def test_mover_archivo_le_mantiene_la_ruta_a_un_archivo_si_se_pasa_la_misma(self):
-    archivo = {"ruta":"/usr/miarchivo.doc", "creacion":"15/02/2019"}
+    archivo = {"percurso":"/usr/miarchivo.doc", "criacao":"15/02/2019"}
     mover_arquivo(archivo, "/usr/miarchivo.doc")
-    self.assertEqual(archivo["ruta"], "/usr/miarchivo.doc")
+    self.assertEqual(archivo["percurso"], "/usr/miarchivo.doc")
 
   def test_mover_archivo_no_modifica_la_fecha_de_creacion_del_archivo(self):
-    archivo = {"ruta":"/usr/miarchivo.doc", "creacion":"15/02/2019"}
+    archivo = {"percurso":"/usr/miarchivo.doc", "criacao":"15/02/2019"}
     mover_arquivo(archivo, "/home/miarchivo.doc")
-    self.assertEqual(archivo["creacion"], "15/02/2019")
+    self.assertEqual(archivo["criacao"], "15/02/2019")
