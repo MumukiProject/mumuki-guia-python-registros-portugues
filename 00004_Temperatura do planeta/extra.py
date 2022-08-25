2b1a58108eea7c81d4828cfcd92aa097b1da15a4
rocio@mumuki.org <rocio@mumuki.org>
2022-08-25 20:39:10 +0000

    Mumuki Export on 2022-08-25 20:39:10 +0000

--- 00004_Temperatura do planeta/extra.py
+++ 00004_Temperatura do planeta/extra.py
@@ -1,4 +1,4 @@
-mercurio = { "nome": "Mercurio", "temperatura_media": 67, "tem_anéis": False }
+mercurio = { "nome": "Mercúrio", "temperatura_media": 67, "tem_anéis": False }
 marte = { "nome": "Marte", "temperatura_media": -63, "tem_anéis": False }
 saturno = { "nome": "Saturno", "temperatura_media": -139, "tem_anéis": True }
 venus = { "nome": "Vênus", "temperatura_media": 462, "tem_anéis": False }
