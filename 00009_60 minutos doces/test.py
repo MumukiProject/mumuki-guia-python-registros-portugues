c55e1c09c21d9dcd7c7e12285ffbd318a28999a0
rocio@mumuki.org <rocio@mumuki.org>
2022-08-25 20:58:01 +0000

    Mumuki Export on 2022-08-25 20:58:01 +0000

--- 00009_60 minutos doces/test.py
+++ 00009_60 minutos doces/test.py
@@ -1,20 +1,20 @@
 
   def test_un_postre_de_una_hora_y_media_no_es_rapido(self):
-    postres_rapidos = [ { "ingredientes": ["galletitas", "dulceDeLeche", "crema"], "tiempo_de_coccion": 20 }, { "ingredientes": ["huevos", "leche", "azúcar", "vainilla"], "tiempo_de_coccion": 50 } ]
-    postre_de_leche = {"ingredientes":["leche"], "tiempo_de_coccion":90}
+    postres_rapidos = [ { "ingredientes": ["galletitas", "dulceDeLeche", "crema"], "tempo_de_cozimento": 20 }, { "ingredientes": ["huevos", "leche", "azúcar", "vainilla"], "tempo_de_cozimento": 50 } ]
+    postre_de_leche = {"ingredientes":["leche"], "tempo_de_cozimento":90}
     adicionar_a_sobremesas_rapidas(postres_rapidos, postre_de_leche)
     self.assertEqual(len(postres_rapidos), 2)
   
   def test_un_postre_de_media_hora_es_rapido(self):
-    postres_rapidos = [ { "ingredientes": ["galletitas", "dulceDeLeche", "crema"], "tiempo_de_coccion": 20 }, { "ingredientes": ["huevos", "leche", "azúcar", "vainilla"], "tiempo_de_coccion": 50 } ]
-    postre_de_leche = {"ingredientes":["leche"], "tiempo_de_coccion":30}
+    postres_rapidos = [ { "ingredientes": ["galletitas", "dulceDeLeche", "crema"], "tempo_de_cozimento": 20 }, { "ingredientes": ["huevos", "leche", "azúcar", "vainilla"], "tempo_de_cozimento": 50 } ]
+    postre_de_leche = {"ingredientes":["leche"], "tempo_de_cozimento":30}
     adicionar_a_sobremesas_rapidas(postres_rapidos, postre_de_leche)
     self.assertEqual(len(postres_rapidos), 3)
     self.assertEqual(postres_rapidos[-1], postre_de_leche)
 
   def test_un_postre_de_una_hora_es_rapido(self):
-    postres_rapidos = [ { "ingredientes": ["galletitas", "dulceDeLeche", "crema"], "tiempo_de_coccion": 20 }, { "ingredientes": ["huevos", "leche", "azúcar", "vainilla"], "tiempo_de_coccion": 50 } ]
-    postre_de_leche = {"ingredientes":["leche"], "tiempo_de_coccion": 60}
+    postres_rapidos = [ { "ingredientes": ["galletitas", "dulceDeLeche", "crema"], "tempo_de_cozimento": 20 }, { "ingredientes": ["huevos", "leche", "azúcar", "vainilla"], "tempo_de_cozimento": 50 } ]
+    postre_de_leche = {"ingredientes":["leche"], "tempo_de_cozimento": 60}
     adicionar_a_sobremesas_rapidas(postres_rapidos, postre_de_leche)
     self.assertEqual(len(postres_rapidos), 3)
     self.assertEqual(postres_rapidos[-1], postre_de_leche)
