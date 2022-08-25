fdf3a9fab01cc4e75405a998bb484ed7073719d7
rocio@mumuki.org <rocio@mumuki.org>
2022-08-25 21:02:05 +0000

    Mumuki Export on 2022-08-25 21:02:05 +0000

--- 00011_Açúcar!/test.py
+++ 00011_Açúcar!/test.py
@@ -1,14 +1,14 @@
 
   def test_endulzar_menu_le_agrega_azucar_al_postre_si_no_tiene(self):
-    menu = {"plato_principal": "bife de lomo", "ensalada": ["papa", "zanahoria", "arvejas"], "postre": { "ingredientes": ["queso crema", "frambuesas"], "tiempo_de_coccion": 80 }}
+    menu = {"prato_principal": "bife de lomo", "salada": ["papa", "zanahoria", "arvejas"], "sobremesa": { "ingredientes": ["queso crema", "frambuesas"], "tiempo_de_coccion": 80 }}
     adocar_menu(menu)
     
-    self.assertEqual(len(menu["postre"]["ingredientes"]), 3)
-    self.assertTrue(menu["postre"]["ingredientes"][-1] == "azúcar" or menu["postre"]["ingredientes"][-1] == "azucar")
+    self.assertEqual(len(menu["sobremesa"]["ingredientes"]), 3)
+    self.assertTrue(menu["sobremesa"]["ingredientes"][-1] == "açúcar" or menu["sobremesa"]["ingredientes"][-1] == "açucar")
 
   def test_endulzar_menu_le_agrega_azucar_al_postre_aun_si_tiene(self):
-    menu = {"plato_principal": "milanesas", "ensalada": ["lechuga", "cebolla"], "postre": { "ingredientes": ["dulce de leche", "vainillas", "azucar"], "tiempo_de_coccion": 60 }}
+    menu = {"prato_principal": "milanesas", "salada": ["lechuga", "cebolla"], "sobremesa": { "ingredientes": ["dulce de leche", "vainillas", "azucar"], "tiempo_de_coccion": 60 }}
     adocar_menu(menu)
     
-    self.assertEqual(len(menu["postre"]["ingredientes"]), 4)
-    self.assertTrue(menu["postre"]["ingredientes"][-1] == "azúcar" or menu["postre"]["ingredientes"][-1] == "azucar")
+    self.assertEqual(len(menu["sobremesa"]["ingredientes"]), 4)
+    self.assertTrue(menu["sobremesa"]["ingredientes"][-1] == "açúcar" or menu["sobremesa"]["ingredientes"][-1] == "açucar")
