c5922814fe52442e0e456f706f49d4125b9dffb9
rocio@mumuki.org <rocio@mumuki.org>
2022-09-26 21:16:00 +0000

    Mumuki Export on 2022-09-26 21:16:00 +0000

--- 00001_Que exercício monumental/corollary.md
+++ 00001_Que exercício monumental/corollary.md
@@ -10,4 +10,17 @@ taj_mahal = { "nombre": "Taj Mahal", "locacion": "Agra, India", "anio_de_constru
 coliseo = { "nombre": "Coliseo", "locacion": "Roma, Italia", "anio_de_construccion": 80 }
 ```
 
+Wow! Muito mais oganizado, não é mesmo? :nerd:
+
+Para modelar estes monumentos as variáveis foram inicializadas assim:
+
+``` python
+estatua_da_liberdade = { "nome": "Estátua da Liberdade", "localizacao": "Nova York, Estados Unidos da América", "ano_da_construcao": 1886 }
+cristo_redentor = { "nome": "Cristo Redentor", "localizacao": "Rio de Janeiro, Brasil", "ano_da_construcao": 1931 }
+torre_eiffel = { "nome": "Torre Eiffel", "localizacao": "Paris, França", "ano_da_construcao": 1889 }
+taj_mahal = { "nome": "Taj Mahal", "localizacao": "Agra, Índia", "ano_da_construcao": 1653 }
+coliseo = { "nome": "Coliseo", "localizacao": "Roma, Itália", "ano_da_construcao": 80 }
+```
+
+
 
--- 00001_Que exercício monumental/extra.py
+++ 00001_Que exercício monumental/extra.py
@@ -1,5 +1,5 @@
-estatua_da_liberdade = { "nome": "Estatua de la Libertad", "localizacao": "Nueva York, Estados Unidos de América", "ano_da_construcao": 1886 }
+estatua_da_liberdade = { "nome": "Estátua da Liberdade", "localizacao": "Nova York, Estados Unidos da América", "ano_da_construcao": 1886 }
 cristo_redentor = { "nome": "Cristo Redentor", "localizacao": "Rio de Janeiro, Brasil", "ano_da_construcao": 1931 }
-torre_eiffel = { "nome": "Torre Eiffel", "localizacao": "París, Francia", "ano_da_construcao": 1889 }
-taj_mahal = { "nome": "Taj Mahal", "localizacao": "Agra, India", "ano_da_construcao": 1653 }
-coliseo = { "nome": "Coliseo", "localizacao": "Roma, Italia", "ano_da_construcao": 80 }
+torre_eiffel = { "nome": "Torre Eiffel", "localizacao": "Paris, França", "ano_da_construcao": 1889 }
+taj_mahal = { "nome": "Taj Mahal", "localizacao": "Agra, Índia", "ano_da_construcao": 1653 }
+coliseo = { "nome": "Coliseo", "localizacao": "Roma, Itália", "ano_da_construcao": 80 }
