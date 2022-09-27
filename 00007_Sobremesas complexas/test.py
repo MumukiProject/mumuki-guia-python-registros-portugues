576f7c2315da922ca3e3b05844b8537680d85950
rocio@mumuki.org <rocio@mumuki.org>
2022-09-27 22:40:46 +0000

    Mumuki Export on 2022-09-27 22:40:46 +0000

--- 00007_Sobremesas complexas/test.py
+++ 00007_Sobremesas complexas/test.py
@@ -4,12 +4,12 @@
     self.cheesecake = { "ingredientes": ["cream cheese", "framboesas"], "tempo_de_cozimento": 80 }
     self.lemon_pie = { "ingredientes": ["suco de limão", "amido de milho", "leite", "ovos"], "tempo_de_cozimento": 65 }
     
-  def test_el_flan_casero_es_mas_dificil_de_cocinar_que_el_cheesecake(self):
+  def test_o_pudim_caseiro_é_mais_difícil_de_cozinhar_do_que_o_cheesecake(self):
     self.assertEqual(mais_dificil_de_cozinhar(self.pudim_caseiro, self.cheesecake), self.pudim_caseiro)
   
-  def test_el_lemon_pie_es_mas_dificil_de_cocinar_que_el_cheesecake(self):
+  def test_o_lemon_pie_é_mais_difícil_de_cozinhar_do_que_o_cheesecake(self):
     self.assertEqual(mais_dificil_de_cozinhar(self.cheesecake, self.lemon_pie), self.lemon_pie)
   
-  def test_si_dos_postres_son_igual_de_dificiles_de_cocinar_devuelve_cualquiera_de_los_dos(self):
+  def test_se_duas_sobremesas_são_igualmente_difíceis_de_cozinhar_retorne_qualquer_uma_das_duas(self):
     mas_dificil = mais_dificil_de_cozinhar(self.pudim_caseiro, self.lemon_pie)
     self.assertTrue(mas_dificil == self.pudim_caseiro or mas_dificil == self.lemon_pie)
