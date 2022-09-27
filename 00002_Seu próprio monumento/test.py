23e1801b6e1cc47b5d80ae56cd1cdee594d0e3cc
rocio@mumuki.org <rocio@mumuki.org>
2022-09-27 21:31:38 +0000

    Mumuki Export on 2022-09-27 21:31:38 +0000

--- 00002_Seu próprio monumento/test.py
+++ 00002_Seu próprio monumento/test.py
@@ -5,14 +5,14 @@
   def test_cristo_redentor_é_do_Rio_de_Janeiro(self):
     self.assertEqual(cristo_redentor["localizacao"], "Rio de Janeiro, Brasil")
 
-  def test_cristo_redentor_tiene_su_anio_de_construccion(self):
+  def test_cristo_redentor_tem_seu_ano_de_construção(self):
     self.assertEqual(cristo_redentor["ano_da_construcao"], 1922)
 
   def test_elevador_lacerda_se_chama_Elevador_Lacerda(self):
     self.assertEqual(elevador_lacerda["nome"].lower(), "Elevador Lacerda")
 
-  def test_elevador_lacerda_é_do_Rosario_Argentina(self):
+  def test_elevador_lacerda_é_de_Salvador_Brasil(self):
     self.assertEqual(elevador_lacerda["localizacao"], "Salvador, Brasil")
 
-  def test_elevador_lacerda_tiene_su_anio_de_construccion(self):
+  def test_elevador_lacerda_tem_seu_ano_de_construção(self):
     self.assertEqual(elevador_lacerda["ano_da_construcao"], 1873)
