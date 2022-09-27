8e55d5bea035de77e537b9c554c5345a6b92abf5
rocio@mumuki.org <rocio@mumuki.org>
2022-09-27 22:39:32 +0000

    Mumuki Export on 2022-09-27 22:39:32 +0000

--- 00006_Dicionários de dois milênios/test.py
+++ 00006_Dicionários de dois milênios/test.py
@@ -1,16 +1,16 @@
 
-  def test_un_archivo_del_2012_no_es_del_milenio_pasado(self):
+  def test_um_arquivo_de_2012_não_é_do_milênio_passado(self):
     archivo = {"percurso":"", "criacao":"01/01/2012"}
     self.assertFalse(e_do_milenio_passado(archivo))
   
-  def test_un_archivo_de_2000_no_es_del_milenio_pasado(self):
+  def test_um_arquivo_de_2000_não_é_do_milênio_passado(self):
     archivo = {"percurso":"", "criacao":"01/01/2000"}
     self.assertFalse(e_do_milenio_passado(archivo))
   
-  def test_un_archivo_de_1999_es_del_milenio_pasado(self):
+  def test_um_arquivo_de_1999_é_do_milênio_passado(self):
     archivo = {"percurso":"", "criacao":"23/09/1994"}
     self.assertTrue(e_do_milenio_passado(archivo))
   
-  def test_un_archivo_de_1994_es_del_milenio_pasado(self):
+  def test_um_arquivo_de_1994_é_do_milênio_passado(self):
     archivo = {"percurso":"", "criacao":"23/09/1994"}
     self.assertTrue(e_do_milenio_passado(archivo))
