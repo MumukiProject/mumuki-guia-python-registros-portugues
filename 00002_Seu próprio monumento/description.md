aedff1e43b38b7eb05e46f87bdacc79654ed6be3
rocio@mumuki.org <rocio@mumuki.org>
2022-09-27 21:30:47 +0000

    Mumuki Export on 2022-09-27 21:30:47 +0000

--- 00002_Seu próprio monumento/description.md
+++ 00002_Seu próprio monumento/description.md
@@ -3,5 +3,5 @@ Os monumentos que você testou no exercício anterior são representados como _d
 Por exemplo, poderíamos armazenar um livro para que cada campo do dicionário fosse alguma característica: o título, autor, data de publicação e muito mais. :books:
 
 
->Agora é o momento do seu monumento! Inicialize as variáveis `cristo_redentor` e `elevador_lacerda` com dicionários desses monumentos, originários das cidades do `Rio de Janeiro` e `Salvador,Bahia` respectivamente. Está empolgado para investigar em que ano eles foram concluídos para completar esse campo? :satisfied:
+>Agora é o momento do seu monumento! Inicialize as variáveis `cristo_redentor` e `elevador_lacerda` com dicionários desses monumentos, originários das cidades do `Rio de Janeiro, Brasil` e `Salvador, Brasil` respectivamente. Está empolgado para investigar em que ano eles foram concluídos para completar esse campo? :satisfied:
 
--- 00002_Seu próprio monumento/test.py
+++ 00002_Seu próprio monumento/test.py
@@ -1,18 +1,18 @@
 
-  def test_torre_azadi_se_llama_Torre_Azadi(self):
-    self.assertEqual(torre_azadi["nome"].lower(), "torre azadi")
+  def test_cristo_redentor_se_chama_Cristo_Redentor(self):
+    self.assertEqual(cristo_redentor["nome"].lower(), "Cristo Redentor")
 
-  def test_torre_azadi_es_de_Teheran_Iran(self):
-    self.assertEqual(torre_azadi["localizacao"], "Teherán, Irán")
+  def test_cristo_redentor_é_do_Rio_de_Janeiro(self):
+    self.assertEqual(cristo_redentor["localizacao"], "Rio de Janeiro, Brasil")
 
-  def test_torre_azadi_tiene_su_anio_de_construccion(self):
-    self.assertEqual(torre_azadi["ano_da_construcao"], 1971)
+  def test_cristo_redentor_tiene_su_anio_de_construccion(self):
+    self.assertEqual(cristo_redentor["ano_da_construcao"], 1922)
 
-  def test_monumento_nacional_a_la_bandera_se_llama_Monumento_Nacional_a_la_Bandera(self):
-    self.assertEqual(monumento_nacional_a_la_bandera["nome"].lower(), "monumento nacional a la bandera")
+  def test_elevador_lacerda_se_chama_Elevador_Lacerda(self):
+    self.assertEqual(elevador_lacerda["nome"].lower(), "Elevador Lacerda")
 
-  def test_monumento_nacional_a_la_bandera_es_de_Rosario_Argentina(self):
-    self.assertEqual(monumento_nacional_a_la_bandera["localizacao"], "Rosario, Argentina")
+  def test_elevador_lacerda_é_do_Rosario_Argentina(self):
+    self.assertEqual(elevador_lacerda["localizacao"], "Salvador, Brasil")
 
-  def test_monumento_nacional_a_la_bandera_tiene_su_anio_de_construccion(self):
-    self.assertEqual(monumento_nacional_a_la_bandera["ano_da_construcao"], 1957)
+  def test_elevador_lacerda_tiene_su_anio_de_construccion(self):
+    self.assertEqual(elevador_lacerda["ano_da_construcao"], 1873)
