e02fddbb0bdd2975a090022ee025c27c9bf58993
rocio@mumuki.org <rocio@mumuki.org>
2022-09-27 22:59:24 +0000

    Mumuki Export on 2022-09-27 22:59:24 +0000

--- 00011_Açúcar!/test.py
+++ 00011_Açúcar!/test.py
@@ -1,12 +1,12 @@
 
-  def test_endulzar_menu_le_agrega_azucar_al_postre_si_no_tiene(self):
+  def test_adoçar_menu_adicionar_açúcar_à_sobremesa_se_não_tiver(self):
     menu = {"prato_principal": "bife de lomo", "salada": ["papa", "zanahoria", "arvejas"], "sobremesa": { "ingredientes": ["queso crema", "frambuesas"], "tiempo_de_coccion": 80 }}
     adocar_menu(menu)
     
     self.assertEqual(len(menu["sobremesa"]["ingredientes"]), 3)
     self.assertTrue(menu["sobremesa"]["ingredientes"][-1] == "açúcar" or menu["sobremesa"]["ingredientes"][-1] == "açucar")
 
-  def test_endulzar_menu_le_agrega_azucar_al_postre_aun_si_tiene(self):
+  def test_adoçar_menu_adicionar_açúcar_à_sobremesa_mesmo_se_tiver(self):
     menu = {"prato_principal": "milanesas", "salada": ["lechuga", "cebolla"], "sobremesa": { "ingredientes": ["dulce de leche", "vainillas", "azucar"], "tiempo_de_coccion": 60 }}
     adocar_menu(menu)
     
