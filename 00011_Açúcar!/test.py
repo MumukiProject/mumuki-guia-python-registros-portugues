6d30797bae6340871810ad3f19e073e322ea1175
franco@mumuki.org <franco@mumuki.org>
2023-01-23 13:22:31 +0000

    Mumuki Export on 2023-01-23 13:22:31 +0000

--- 00011_Açúcar!/test.py
+++ 00011_Açúcar!/test.py
@@ -4,11 +4,19 @@
     adocar_menu(menu)
     
     self.assertEqual(len(menu["sobremesa"]["ingredientes"]), 3)
-    self.assertTrue(menu["sobremesa"]["ingredientes"][-1] == "açúcar" or menu["sobremesa"]["ingredientes"][-1] == "açucar")
+    self.assertTrue(
+      menu["sobremesa"]["ingredientes"][-1] == "açúcar"
+      or menu["sobremesa"]["ingredientes"][-1] == "açucar"
+      or menu["sobremesa"]["ingredientes"][-1] == "acucar"
+    )
 
   def test_adoçar_menu_adicionar_açúcar_à_sobremesa_mesmo_se_tiver(self):
     menu = {"prato_principal": "milanesas", "salada": ["lechuga", "cebolla"], "sobremesa": { "ingredientes": ["dulce de leche", "vainillas", "azucar"], "tiempo_de_coccion": 60 }}
     adocar_menu(menu)
     
     self.assertEqual(len(menu["sobremesa"]["ingredientes"]), 4)
-    self.assertTrue(menu["sobremesa"]["ingredientes"][-1] == "açúcar" or menu["sobremesa"]["ingredientes"][-1] == "açucar")
+    self.assertTrue(
+      menu["sobremesa"]["ingredientes"][-1] == "açúcar"
+      or menu["sobremesa"]["ingredientes"][-1] == "açucar"
+      or menu["sobremesa"]["ingredientes"][-1] == "acucar"
+    )
