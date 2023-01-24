0ef428ead11a49adb44f79b799e2705745453c0e
franco@mumuki.org <franco@mumuki.org>
2023-01-24 20:57:00 +0000

    Mumuki Export on 2023-01-24 20:57:00 +0000

--- 00007_Sobremesas complexas/extra.py
+++ 00007_Sobremesas complexas/extra.py
@@ -1,4 +1,4 @@
 pudim_caseiro = { "ingredientes": ["ovos", "leite", "açúcar", "baunilha"], "tempo_de_cozimento": 50 }
 cheesecake = { "ingredientes": ["cream cheese", "framboesas"], "tempo_de_cozimento": 80 }
 lemon_pie = { "ingredientes": ["suco de limão", "amido de milho", "leite", "ovos"], "tempo_de_cozimento": 65 }
-cheesecake_extra_dolce = { "ingredientes": ["cream cheese", "framboesas", "açúcar"], "tempo_de_cozimento": 80 }
+cheesecake_extra_doce = { "ingredientes": ["cream cheese", "framboesas", "açúcar"], "tempo_de_cozimento": 80 }
