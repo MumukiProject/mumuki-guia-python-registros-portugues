3d395f814f1ba969156007021c48ae910628507c
franco@mumuki.org <franco@mumuki.org>
2023-01-24 21:06:06 +0000

    Mumuki Export on 2023-01-24 21:06:06 +0000

--- 00007_Sobremesas complexas/test.py
+++ 00007_Sobremesas complexas/test.py
@@ -11,6 +11,18 @@
   def test_o_lemon_pie_é_mais_difícil_de_cozinhar_do_que_o_cheesecake_com_argumentos_trocados(self):
     self.assertEqual(mais_dificil_de_cozinhar(lemon_pie, cheesecake), lemon_pie, "mais_dificil_de_cozinhar(lemon_pie, cheesecake) deve retornar lemon_pie")
 
+  def test_o_pudim_caseiro_é_mais_difícil_de_cozinhar_do_que_o_cheesecake_extra_doce(self):
+    self.assertEqual(mais_dificil_de_cozinhar(pudim_caseiro, cheesecake_extra_doce), pudim_caseiro, "mais_dificil_de_cozinhar(pudim_caseiro, cheesecake_extra_doce) deve retornar pudim_caseiro (porque tem mais um ingrediente)")
+
+  def test_o_pudim_caseiro_é_mais_difícil_de_cozinhar_do_que_o_cheesecake_extra_doce_com_argumentos_trocados(self):
+    self.assertEqual(mais_dificil_de_cozinhar(cheesecake_extra_doce, pudim_caseiro), pudim_caseiro, "mais_dificil_de_cozinhar(cheesecake_extra_doce, pudim_caseiro) deve retornar pudim_caseiro (porque tem mais um ingrediente)")
+
+  def test_o_cheesecacke_extra_doce_é_mais_difícil_de_cozinhar_do_que_o_cheesecake(self):
+    self.assertEqual(mais_dificil_de_cozinhar(cheesecake, cheesecacke_extra_doce), cheesecacke_extra_doce, "mais_dificil_de_cozinhar(cheesecake, cheesecacke_extra_doce) deve retornar cheesecacke_extra_doce (porque tem mais um ingrediente)")
+
+  def test_o_cheesecacke_extra_doce_é_mais_difícil_de_cozinhar_do_que_o_cheesecake_com_argumentos_trocados(self):
+    self.assertEqual(mais_dificil_de_cozinhar(cheesecacke_extra_doce, cheesecake), cheesecacke_extra_doce, "mais_dificil_de_cozinhar(cheesecacke_extra_doce, cheesecake) deve retornar cheesecacke_extra_doce (porque tem mais um ingrediente)")
+
   def test_se_duas_sobremesas_são_igualmente_difíceis_de_cozinhar_retorne_qualquer_uma_das_duas(self):
     mas_dificil = mais_dificil_de_cozinhar(pudim_caseiro, lemon_pie)
     self.assertTrue(mas_dificil == pudim_caseiro or mas_dificil == lemon_pie)
